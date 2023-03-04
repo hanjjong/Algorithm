9536525015e0fe60be2ab1cb82035a4813879536
Hanjjong <tim4974@naver.com>
2023-03-04 17:15:24 +0900

    added

--- BEAKJOON/자료구조/1966-프린터 큐.py
+++ BEAKJOON/자료구조/1966-프린터 큐.py
@@ -1,36 +1,22 @@
 import sys
 from collections import deque
 
-queue = deque()
-def que():
-    global count
-    while queue:
-        x = queue.popleft()
-        for i in queue:
-            print(i)
-            if x < i:
-                queue.append(x)
-                count += 1
-            else :
-                return i
-        
-
 num = int(input())
 
-important = []
-print_out = []
 for i in range(num):
-    count = 1
-    queue.clear()
-    n, when = map(int,sys.stdin.readline().strip().split())
-    important.append(list(map(int, sys.stdin.readline().strip().split())))
-
-    for j in range(n):
-        queue.append(important[i][j])
-    print(queue)
-    if que() == important[when]:
-        print_out.append(count)
-        print(count)
-
-for i in range(num):
-    print(print_out[i])
+    n, m = list(map(int, input().split()))
+    queue = deque(list(map(int,sys.stdin.readline().strip().split())))
+    count = 0
+    while queue:
+        max_q = max(queue)
+        x = queue.popleft()
+        m -= 1
+        if x == max_q:
+            count += 1
+            if m < 0 :
+                print(count)
+                break
+        else:
+            queue.append(x)
+            if m < 0:
+                m = len(queue) - 1
