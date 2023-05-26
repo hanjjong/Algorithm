0cb963746137ab97a5c66ff40aa2c6d6bbaaafdd
Hanjjong <tim4974@naver.com>
2023-05-27 03:53:46 +0900

    added

---
+++ BEAKJOON/자료구조/10773-제로.py
@@ -0,0 +1,12 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+stack = []
+for i in range(T):
+    num = int(sys.stdin.readline())
+    if num != 0 :
+        stack.append(num)
+    else :
+        stack.pop()
+print(sum(stack))
