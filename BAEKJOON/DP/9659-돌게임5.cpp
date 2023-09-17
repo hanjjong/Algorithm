1efe9cc9991743ccffd223a8366efe0ee9d89f77
Hanjjong <tim4974@naver.com>
2023-09-17 17:06:56 +0900

    [feat] : create 9659

---
+++ BAEKJOON/DP/9659-돌게임5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned long long num;
+    cin >> num;
+    if (num % 2 == 0)
+        cout << "CY";
+    else
+        cout << "SK";
+}
