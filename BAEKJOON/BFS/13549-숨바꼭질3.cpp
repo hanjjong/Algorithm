91f16efb654360c0254d6ff5ecdb75587f6850f1
Hanjjong <tim4974@naver.com>
2024-08-11 19:32:20 +0900

    [feat] : feat 13549

--- BAEKJOON/BFS/13549-숨바꼭질3.cpp
+++ BAEKJOON/BFS/13549-숨바꼭질3.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <queue>
 
-int arr[1000003];
-int visited[1000003];
+int arr[100003];
+int visited[100003];
 std::queue<int> q;
 
 int subin;
@@ -12,31 +12,37 @@ int dx[] = {1, -1};
 int main(){
 
     std::cin >> subin >> bro;
+    std::fill(visited, visited + 100001, 2147483647); // 방문 배열 초기화
+    visited[subin] = 0;
     q.push(subin);
     while (!q.empty()){
         int cur = q.front();
         q.pop();
+
+        if (cur == bro) {  // 동생을 찾으면 종료
+            std::cout << visited[cur];
+            return 0;
+        }
         for (int i = 0; i < 3; i++){
             int nx;
-            if (i < 2)
-                nx = cur + dx[i];     
-            else if (i == 2)
+            if (i == 0)
                 nx = cur * 2;
+            else
+                nx = cur + dx[i - 1];     
 
-            if (nx < 0 || nx >= 100000) continue;
-            if (i < 2) {
-                if (visited[nx] > visited[cur] + 1 || visited[nx] == 0){
+            if (nx < 0 || nx >= 100001) continue;
+            if (i > 0) {
+                if (visited[nx] > visited[cur] + 1){
                     visited[nx] = visited[cur] + 1;
                     q.push(nx);
                 }
             }
-            else if (i == 2){
-                if (visited[nx] > visited[cur] || visited[nx] == 0){
+            else if (i == 0){
+                if (visited[nx] > visited[cur]){
                     visited[nx] = visited[cur];
                     q.push(nx);
                 }
             }
         }
     }
-    std::cout << visited[bro];
 }
