369c6493e7cf316d459131453f959f6d8e578a30
Hanjjong <tim4974@naver.com>
2024-08-13 21:41:46 +0900

    [feat] : feat 15651

---
+++ BAEKJOON/백트래킹/15650-N과M.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+//  3  1
+int n, m;
+int is_used[9];
+int arr[9];
+
+void recul(int num) {
+    if (num == m) {
+        for (int i = 0; i < m; i++){
+            std::cout << arr[i] << " ";
+        }
+        std::cout << "\n";
+        return ;
+    }
+
+    int st = 1; // 시작지점, k = 0일 때에는 st = 1
+    if(num != 0) st = arr[num-1] + 1;
+    for (int i = st; i <= n; i++) {
+        if (!is_used[i]){
+            arr[num] = i;
+            is_used[num + 1] = 1;
+            recul(num + 1);
+            is_used[num + 1] = 0;
+        }
+    }
+    return ;
+}
+
+int main() {
+    std::cin >> n >> m;
+    recul(0);
+}
